c747e38521a7b35ecb0ce226c34b011880e2d5a6
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-04-01 23:03:13 -0600

    25148

---
+++ MAP Clase Marzo 19 2025/MAP Clase Marzo 19 2025/MAP Clase Marzo 19 2025.cpp
@@ -0,0 +1,30 @@
+// MAP Clase Marzo 19 2025.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <vector>
+#include <map>
+using std::vector;
+
+int main()
+{
+	std::map<int, int> alumno;
+	alumno[007] = 6;
+	alumno[001] = 4;
+	alumno[003] = 5;
+
+	for (std::map<int, int>::iterator i = alumno.begin(); i != alumno.end(); ++i) {
+		std::cout << i->second << std::endl;
+	}
+	return 1;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
